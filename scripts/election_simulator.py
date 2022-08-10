cb287d5bb227e516c651749f87d56754231475b4
Sean Malcolm <anon@gmail.com>
2022-08-10 20:15:05 +1000

    wrote election simulator

--- scripts/election_simulator.py
+++ scripts/election_simulator.py
@@ -1,27 +1,83 @@
+import voter_generation
 
+import math
 
 
 class Election:
     
-    def __init__(self, voters : list):
+    
+    def __init__(self, voters : list, parties : list):
         
         self.voters = voters
+        self.parties = parties
         
         
     def run_election(self):
+
+        current_votes = {party : [] for party in self.parties}
+                         
+        N = len(self.voters)
         
-        pass
-
-
-
-
-
-
-
+        stage = 0
+        while True:
+            if stage == 0:
+                for voter in self.voters:
+                    current_votes[voter.ballot[0]].append(voter)
+            else:
+                #Check if over
+                if self.get_current_winner(current_votes)['votes'] > N/2:
+                    break
+                else:
+                    eliminated = self.get_current_loser(current_votes)['party']
+                    for i, voter in enumerate(current_votes[eliminated]):
+                        current = voter.ballot.index(eliminated)
+                        i = current + 1
+                        while i < len(voter.ballot):
+                            next_preferred = voter.ballot[i]
+                            if next_preferred in current_votes:
+                                current_votes[next_preferred].append(voter)
+                                break
+                            else:
+                                i += 1  
+                current_votes[eliminated] = []
+                
+            for party in current_votes:
+                print(f'------Stage {stage}---------')
+                print(f'{party} : {len(current_votes[party])}')
+            stage += 1
+            
+    def get_current_winner(self, votes):
+        max_i = None
+        max_v = 0
+        for party in votes:
+            if len(votes[party]) > max_v:
+                max_i = party
+                max_v = len(votes[party])   
+        return({'party' : max_i, 'votes' : max_v})
+    
+    def get_current_loser(self, votes):
+        
+        min_i = None
+        min_v = math.inf
+        for party in votes:
+            if len(votes[party]) < min_v:
+                min_i = party
+                min_v = len(votes[party])
+        return({'party' : min_i, 'votes' : min_v})
+    
 
 
 if __name__ == '__main__':
     
     
     
-    pass
+    senate_votes = voter_generation.read_senate_data()
+    print("data read")
+    voters = voter_generation.generate_voters(senate_votes)
+    print('voters_generated')
+    relevant_parties = voter_generation.get_relevant_parties()
+    
+    election = Election(voters=voters, parties=relevant_parties)
+    election.run_election()
+    
+    
--- scripts/voter_generation.py
+++ scripts/voter_generation.py
@@ -1,16 +1,22 @@
 import os
 
+
 import pandas as pd
 
 
 
+def get_relevant_parties():
+    data_folder = os.path.join(os.path.dirname(os.getcwd()), 'data')
+    relevant_parties = []
+    with open(f'{data_folder}/relevant_parties.txt', 'r') as file:
+        for line in file.readlines():
+            relevant_parties.append(line.rstrip('\n'))
+    return relevant_parties
+
+
 class Voter:
     
-    relevant_parties = [
-        'D:Liberal/The Nationals',
-        'K:Australian Labor Party',
-        'U:The Greens'                                                    
-        ]
+    relevant_parties = get_relevant_parties()
     
     def __init__(self, senate_ballot : pd.Series, division : str, collection_point : str):
         
@@ -23,12 +29,14 @@ class Voter:
         
         ballot = ballot.sort_values()
         return(list(ballot.index))
-        
+    
     
 def generate_voters(senate_votes : pd.DataFrame):
     
     voters = []
     for index, row in senate_votes.iterrows():
+        if index % 10000 == 0:
+            print(f'Generating voters {index} / {senate_votes.shape[0]}')
         division = row['Division']
         collection_point = row['Vote Collection Point Name']
         ballot = row[Voter.relevant_parties]
@@ -40,12 +48,11 @@ def generate_voters(senate_votes : pd.DataFrame):
 def read_senate_data():
     
     data_folder = os.path.join(os.path.dirname(os.getcwd()), 'data')
-    senate_votes = pd.read_csv(f"{data_folder}/aec-senate-formalpreferences-27966-VIC.csv", nrows=100)
+    senate_votes = pd.read_csv(f"{data_folder}/aec-senate-formalpreferences-27966-VIC.csv", nrows=200000)
     return senate_votes
 
 
 if __name__ == '__main__':
     
-
     senate_votes = read_senate_data()
     generate_voters(senate_votes)
